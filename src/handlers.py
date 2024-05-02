862bb4858783bb1a2ded7b1363ec05a4361f1f82
Aleksey <ad.sychev22@gmail.com>
2024-05-02 13:50:31 +0300

    some changes

--- src/handlers.py
+++ src/handlers.py
@@ -143,7 +143,7 @@ async def order_done(message: Message):
         logger.info(f"Query: {session.query(Order.message_id).filter(Order.released.is_(False)).all()}")
         if (message.reply_to_message.message_id,) in session.query(Order.message_id).filter(Order.released.is_(False)).all():
             logger.info("success")
-            bot.unpin_chat_message(WORKERS_GROUP_ID, message.reply_to_message.message_id)
+            await bot.unpin_chat_message(WORKERS_GROUP_ID, message.reply_to_message.message_id)
             order = session.query(Order).filter(Order.message_id == message.reply_to_message.message_id).one()
             order.released = True
             order.release_date = datetime.datetime.now()
